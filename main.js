07303337a4508751407ecba71557ea67157ac095
comsynth <thatcher@comsynth.com>
2020-05-15 13:57:12 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,234 @@
+//Add a question to the form: “Would you recommend this product”. Then take in that response from the user via radio buttons of “yes” or “no” and add it to the productReview object, with form validation.
+Vue.component('navbar', {
+    template: `
+     <div class="nav-bar"><div class="nav-brand"> comsynth vue app </div></div>
+     `,
+})
+Vue.component('footer2', {
+    template: `
+    <div class="nav-bar"><div class="nav-brand"> thatcher@comsynth.com // 615 943 2197 </div></div>
+     `,
+})
+Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template: `
+     <div class="product">
+          
+        <div class="product-image">
+          <img :src="image" />
+        </div>
+  
+        <div class="product-info">
+            <h1>{{ product }}</h1>
+            <p v-if="inStock">In Stock</p>
+            <p v-else>Out of Stock</p>
+            <p>Shipping: {{ shipping }}</p>
+  
+            <ul>
+              <li v-for="detail in details">{{ detail }}</li>
+            </ul>
+            <ul>
+              <li v-for="size in sizes">{{ size }}</li>
+            </ul>
+  
+            <div class="color-box"
+                 v-for="(variant, index) in variants" 
+                 :key="variant.variantId"
+                 :style="{ backgroundColor: variant.variantColor }"
+                 v-on:click="updateProduct(index)"
+                 >
+            </div> 
+  
+            <button v-on:click="addToCart" 
+              :disabled="!inStock"
+              :class="{ disabledButton: !inStock }"
+              >
+            Add to cart
+            </button>
+
+
+            <button type="button" data-toggle="collapse" data-target="#review" aria-expanded="false" aria-controls="collapseExample">
+            write review
+          </button>
+  
+         </div> 
+
+          <div>
+              <p v-if="!reviews.length">There are no reviews yet.</p>
+              <ul v-else>
+                  <li v-for="(review, index) in reviews" :key="index">
+                    <p>{{ review.name }}</p>
+                    <p>Rating:{{ review.rating }}</p>
+                    <p>{{ review.review }}</p>
+                  </li>
+              </ul>
+          </div>
+         
+         <product-review @review-submitted="addReview"></product-review>
+      
+      </div>
+     `,
+    data() {
+        return {
+            product: 'Socks',
+            selectedVariant: 0,
+            details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+            sizes: ['Small', 'Medium', 'Large'],
+            variants: [{
+                    variantId: 2234,
+                    variantColor: 'green',
+                    variantImage: '/images/jdleins.jpg',
+                    variantQuantity: 10
+                },
+                {
+                    variantId: 2235,
+                    variantColor: 'blue',
+                    variantImage: '/images/mrshacklett.jpg',
+                    variantQuantity: 0
+                }
+            ],
+            reviews: []
+        }
+    },
+    methods: {
+        addToCart() {
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)
+        },
+        updateProduct(index) {
+            this.selectedVariant = index
+        },
+        addReview(productReview) {
+            this.reviews.push(productReview)
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        shipping() {
+            if (this.premium) {
+                return "Free"
+            }
+            return "$1.99"
+        }
+    }
+})
+
+
+Vue.component('product-review', {
+    template: `
+      <form class="review-form" @submit.prevent="onSubmit">
+     
+      
+        
+       
+
+        <p class="error" v-if="errors.length">
+        <b>Please correct the following error(s):</b>
+        <ul>
+          <li v-for="error in errors">{{ error }}</li>
+        </ul>
+      </p>
+     
+      <div class="collapse" id="review">
+        <div class="card card-body">
+        <p>
+          <label for="name">Name:</label>
+          <input id="name" v-model="name">
+        </p>
+        
+        <p>
+          <label for="review">Review:</label>      
+          <textarea id="review" v-model="review"></textarea>
+        </p>
+        
+        <p>
+          <label for="rating">Rating:</label>
+          <select id="rating" v-model.number="rating">
+            <option>5</option>
+            <option>4</option>
+            <option>3</option>
+            <option>2</option>
+            <option>1</option>
+          </select>
+        </p>
+
+        <p>Would you recommend this product?</p>
+        <label>
+          Yes
+          <input type="radio" value="Yes" v-model="recommend"/>
+        </label>
+        <label>
+          No
+          <input type="radio" value="No" v-model="recommend"/>
+        </label>
+            
+        <p>
+          <input type="submit" value="Submit">  
+        </p>    
+        </div>
+      </div>
+
+
+
+        
+      
+    </form>
+    `,
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null,
+            recommend: null,
+            errors: []
+        }
+    },
+    methods: {
+        onSubmit() {
+            this.errors = []
+            if (this.name && this.review && this.rating && this.recommend) {
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating,
+                    recommend: this.recommend
+                }
+                this.$emit('review-submitted', productReview)
+                this.name = null
+                this.review = null
+                this.rating = null
+                this.recommend = null
+            } else {
+                if (!this.name) this.errors.push("Name required.")
+                if (!this.review) this.errors.push("Review required.")
+                if (!this.rating) this.errors.push("Rating required.")
+                if (!this.recommend) this.errors.push("Recommendation required.")
+            }
+        }
+    }
+})
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        premium: false,
+        cart: []
+    },
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+        }
+    }
+})
